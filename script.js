4295d0c4a75c5334e71cdf711128f0adb8aec6a7
Razennara <rattawut.ko@ku.th>
2025-02-14 05:39:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function toggleLetter() {
-    let letter = document.querySelector(".valentines");
-    letter.classList.toggle("open");
-}
+$(document).ready(function () {
+  $(".container")
+    .mouseenter(function () {
+      $(".card").stop().animate(
+        {
+          top: "-90px",
+        },
+        "slow"
+      );
+    })
+    .mouseleave(function () {
+      $(".card").stop().animate(
+        {
+          top: 0,
+        },
+        "slow"
+      );
+    });
+});
